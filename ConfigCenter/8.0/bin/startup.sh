dcdd111142ed01940a571116120832649f1948f6
recallcode <recallcode@aliyun.com>
2018-09-11 22:26:26 +0800

    ConfigCenter

---
+++ ConfigCenter/8.0/bin/startup.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+## resolve links
+PRG="$0"
+
+# need this for relative symlinks
+while [ -h "$PRG" ] ; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG="`dirname "$PRG"`/$link"
+  fi
+done
+
+saveddir=`pwd`
+
+AW_COMPONENT_HOME=`dirname "$PRG"`/..
+
+# make it fully qualified
+AW_COMPONENT_HOME=`cd "$AW_COMPONENT_HOME" && pwd`
+
+cd "$saveddir"
+
+
+
+${AW_COMPONENT_HOME}/bin/build.sh start
